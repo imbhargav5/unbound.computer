aa0a64a2365e5a993bc67c0b94516378142b08ba
Bhargav Ponnapalli <imbhargav5@gmail.com>
2024-07-23 21:21:25 +0530

    update test case

--- e2e/anon/home.spec.ts
+++ e2e/anon/home.spec.ts
@@ -3,6 +3,6 @@ import { expect, test } from '@playwright/test';
 test('should have title in home page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('/')
-  await expect(page.locator('h1')).toContainText('Nextbase Demo Landing Page')
+  await expect(page.locator('h1')).toContainText('Nextbase Ultimate Landing Page')
 });
 
