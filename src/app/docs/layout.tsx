627cf7b408f3f2978936ee916b3a2f5522678eb5
Bhargav Ponnapalli <imbhargav5@gmail.com>
2024-09-05 10:33:56 +0530

    page trees in fumadocs

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,11 +1,37 @@
 import { pageTree } from '@/app/source';
+import { RootToggle } from 'fumadocs-ui/components/layout/root-toggle';
 import { DocsLayout } from 'fumadocs-ui/layout';
+import { Home } from 'lucide-react';
 import type { ReactNode } from 'react';
 
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
     <DocsLayout tree={pageTree}
+      sidebar={{
+        banner: (
+          <RootToggle
+            options={[
+              {
+                title: 'Page Tree 1',
+                description: 'Pages in page tree 1',
+                url: '/docs/page-tree-1',
+                icon: <Home />,
+              },
+              {
+                title: 'Page Tree 2',
+                description: 'Pages in page tree 2',
+                url: '/docs/page-tree-2',
+                icon: <Home />,
+              },
+            ]}
+          />
+        ),
+      }}
       links={[
+        {
+          text: 'Home',
+          url: '/',
+        },
         {
           text: 'Blog',
           url: '/blog',
@@ -13,8 +39,8 @@ export default function RootDocsLayout({ children }: { children: ReactNode }) {
         },
       ]}
       nav={{
-        title: 'Nextbase Ultimate',
-        url: '/',
+        title: 'Nextbase Docs',
+        url: '/docs',
       }}>
       {children}
     </DocsLayout>
