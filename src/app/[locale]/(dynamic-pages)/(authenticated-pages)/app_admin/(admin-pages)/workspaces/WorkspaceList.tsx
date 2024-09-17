81ec44b11eeea28059823e99144dd9f058e0c4a1
Bhargav Ponnapalli <imbhargav5@gmail.com>
2024-09-17 10:14:19 +0530

    workspaces

--- src/app/[locale]/(dynamic-pages)/(authenticated-pages)/app_admin/(admin-pages)/workspaces/WorkspaceList.tsx
+++ src/app/[locale]/(dynamic-pages)/(authenticated-pages)/app_admin/(admin-pages)/workspaces/WorkspaceList.tsx
@@ -1,4 +1,5 @@
 'use server';
+
 import {
   Table as ShadcnTable,
   TableBody,
@@ -9,6 +10,7 @@ import {
 } from '@/components/ui/table';
 import { getPaginatedWorkspaceListAction } from '@/data/admin/workspaces';
 import { format } from 'date-fns';
+import Link from 'next/link';
 import { AppAdminWorkspacesFiltersSchema } from './schema';
 
 export async function WorkspaceList({
@@ -16,35 +18,53 @@ export async function WorkspaceList({
 }: {
   filters: AppAdminWorkspacesFiltersSchema;
 }) {
-  const workspaces = await getPaginatedWorkspaceListAction(filters);
-  return (
-    <div className="rounded-lg overflow-hidden border">
-      <ShadcnTable>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Slug</TableHead>
-            <TableHead>Created At</TableHead>
-            <TableHead>Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {workspaces.map((workspace) => (
-            <TableRow key={workspace.id}>
-              <TableCell>{workspace.name ?? '-'}</TableCell>
-              <TableCell>{workspace.slug ?? '-'}</TableCell>
-              <TableCell>
-                {format(new Date(workspace.created_at), 'PPpp')}
-              </TableCell>
-              <TableCell>
-                <span className="flex items-center space-x-2">
-                  {/* Add actions here if needed */}
-                </span>
-              </TableCell>
+  const workspacesActionResult = await getPaginatedWorkspaceListAction(filters);
+  console.log("workspacesActionResult", workspacesActionResult);
+
+  if (workspacesActionResult?.data) {
+    const workspaces = workspacesActionResult.data;
+    return (
+      <div className="space-y-2 rounded-lg overflow-hidden border">
+        <ShadcnTable>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Slug</TableHead>
+              <TableHead>Created At</TableHead>
+              <TableHead>Actions</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </ShadcnTable>
-    </div>
-  );
+          </TableHeader>
+          <TableBody>
+            {workspaces.map((workspace) => (
+              <TableRow key={workspace.id}>
+                <TableCell>
+                  <Link href={`/app_admin/workspaces/${workspace.id}`}>
+                    {workspace.name ?? '-'}
+                  </Link>
+                </TableCell>
+                <TableCell>{workspace.slug ?? '-'}</TableCell>
+                <TableCell>
+                  {format(new Date(workspace.created_at), 'PPpp')}
+                </TableCell>
+                <TableCell>
+                  <span className="flex items-center space-x-2">
+                    {/* Add actions here if needed */}
+                  </span>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </ShadcnTable>
+      </div>
+    );
+  } else {
+    if (workspacesActionResult?.serverError) {
+      console.log("***************");
+      console.log(workspacesActionResult.serverError);
+      return <div>{workspacesActionResult.serverError}</div>
+    } else {
+      console.error(workspacesActionResult);
+      return <div>Failed to load workspaces</div>
+    }
+  }
 }
