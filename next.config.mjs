21097bee53831d35e483fdb59cfe8a83b1f88af0
Bhargav Ponnapalli <imbhargav5@gmail.com>
2024-09-23 14:44:07 +0530

    content collections fixes

--- next.config.mjs
+++ next.config.mjs
@@ -63,23 +63,8 @@ const nextConfig = {
   },
 
   reactStrictMode: true,
-  // webpack: (config) => {
-  //   if (typeof nextRuntime === 'undefined') {
-  //     config.resolve.fallback = {
-  //       ...config.resolve.fallback,
-  //       fs: false,
-  //     };
-  //   }
 
-  //   return config;
-  // },
   sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
     hideSourceMaps: true,
   },
   logging: {
