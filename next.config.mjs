f698ca9802dbb0a482969de979876ccc697d1cde
Bhargav Ponnapalli <imbhargav5@gmail.com>
2024-09-25 15:42:06 +0530

    remove unnecessary config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { withContentCollections } from '@content-collections/next';
 import { withSentryConfig } from '@sentry/nextjs';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+const withNextIntl = createNextIntlPlugin();
 
 import createWithBundleAnalyzer from '@next/bundle-analyzer';
 
