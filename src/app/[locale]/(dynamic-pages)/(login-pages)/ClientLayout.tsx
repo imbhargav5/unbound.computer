bfd9584bfb91a0126b68d440b22516edfda3353f
Bhargav Ponnapalli <imbhargav5@gmail.com>
2024-10-12 00:36:55 +0530

    login page updates

--- src/app/[locale]/(dynamic-pages)/(login-pages)/ClientLayout.tsx
+++ src/app/[locale]/(dynamic-pages)/(login-pages)/ClientLayout.tsx
@@ -7,11 +7,11 @@ import { StackedCards } from "./StackedCards";
 export function ClientLayout({ children }: { children: ReactNode }) {
   return (
     <div className="lg:flex h-screen border-2 border-black">
-      <div className="lg:w-1/2 p-8 flex items-center relative justify-center h-full bg-background">
+      <div className="lg:w-2/5 p-8 flex items-center relative justify-center h-full bg-background">
         <div className="max-w-xl md:min-w-[450px]">{children}</div>
       </div>
-      <div className="hidden lg:flex w-1/2 border-l-2 items-center relative justify-center h-full">
-        <div className="w-full px-8">
+      <div className="hidden lg:flex w-3/5 border-l-2 items-center relative justify-center h-full">
+        <div className="w-full px-32">
           <StackedCards
             images={[
               "/assets/marketing/landing-1.jpg",
