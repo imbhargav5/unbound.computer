6d8f0026633b703048d23ca775955b2f52fe103b
Bhargav Ponnapalli <imbhargav5@gmail.com>
2024-11-13 13:17:58 +0530

    add an oops page

--- src/app/[locale]/(dynamic-pages)/(authenticated-pages)/dashboard/route.ts
+++ src/app/[locale]/(dynamic-pages)/(authenticated-pages)/dashboard/route.ts
@@ -9,7 +9,7 @@ export async function GET(req: NextRequest) {
   try {
     const initialWorkspace = await getMaybeDefaultWorkspace();
     if (!initialWorkspace) {
-      return NextResponse.redirect(toSiteURL("/404"));
+      return NextResponse.redirect(toSiteURL("/oops"));
     }
     console.log("initialWorkspace", initialWorkspace.workspace.membershipType);
     return NextResponse.redirect(
@@ -21,6 +21,6 @@ export async function GET(req: NextRequest) {
   } catch (error) {
     console.error("Failed to load dashboard:", error);
     // Redirect to an error page or show an error message
-    return NextResponse.redirect(toSiteURL("/404"));
+    return NextResponse.redirect(toSiteURL("/oops"));
   }
 }
