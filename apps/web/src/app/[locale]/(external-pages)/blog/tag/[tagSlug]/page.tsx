e69d5189a1ffe0ad24835e1a27c439bb9b6a0142
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2025-12-14 15:58:05 +0530

    refactor: comment out generateStaticParams function to prevent cache failures
    - Commented out the `generateStaticParams` function in the blog tag page to avoid issues when no tags are present in the database.
    - Removed related code for generating parameters based on tags, ensuring the function does not execute in its current state.

--- apps/web/src/app/[locale]/(external-pages)/blog/tag/[tagSlug]/page.tsx
+++ apps/web/src/app/[locale]/(external-pages)/blog/tag/[tagSlug]/page.tsx
@@ -9,7 +9,6 @@ import {
   cachedGetPublishedBlogPostsByTagSlug,
   cachedGetTagBySlug,
 } from "@/data/anon/marketing-blog";
-import { routing } from "@/i18n/routing";
 import { PublicBlogList } from "../../public-blog-list";
 import { TagsNav } from "../../tags-nav";
 
@@ -18,15 +17,17 @@ const BlogListByTagPageParamsSchema = z.object({
   locale: z.string(),
 });
 
-export async function generateStaticParams() {
-  const tags = await cachedGetAllBlogTags();
+// This async functionwill fail in cache components if there
+// are no tags in the database. Comment it if so.
+// export async function generateStaticParams() {
+//   const tags = await cachedGetAllBlogTags();
 
-  const params = tags.flatMap((tag) =>
-    routing.locales.map((locale) => ({ tagSlug: tag.slug, locale }))
-  );
+//   const params = tags.flatMap((tag) =>
+//     routing.locales.map((locale) => ({ tagSlug: tag.slug, locale }))
+//   );
 
-  return params;
-}
+//   return params;
+// }
 
 export async function generateMetadata(props: {
   params: Promise<unknown>;
