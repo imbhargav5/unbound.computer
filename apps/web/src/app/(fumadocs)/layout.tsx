8e5ca3d7d9371515de9e5ebc49f32230becac4f6
Ponnapalli Bhargav <bhargavponnapalli.5@gmail.com>
2026-02-17 14:44:38 +0530

    Implement UNB-91 docs external shell

--- apps/web/src/app/(fumadocs)/layout.tsx
+++ apps/web/src/app/(fumadocs)/layout.tsx
@@ -3,6 +3,8 @@ import { baseOptions } from "@/app/layout.config";
 import { source } from "../source";
 import "@/styles/docs-layout-styles.css";
 import { RootProvider } from "fumadocs-ui/provider/next";
+import { ExternalNavigation } from "@/components/navigation-menu/external-navbar/external-navigation";
+import { Footer } from "@/components/landing-page/footer";
 
 export default async function Layout({
   children,
@@ -10,10 +12,18 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <RootProvider>
-      <DocsLayout tree={source.pageTree} {...baseOptions()}>
-        {children}
-      </DocsLayout>
-    </RootProvider>
+    <div className="dark flex min-h-screen flex-col bg-black text-white">
+      <ExternalNavigation />
+      <div className="flex-1">
+        <div className="mx-auto w-full max-w-6xl px-6 pt-16 md:px-8">
+          <RootProvider>
+            <DocsLayout tree={source.pageTree} {...baseOptions()}>
+              {children}
+            </DocsLayout>
+          </RootProvider>
+        </div>
+      </div>
+      <Footer />
+    </div>
   );
 }
