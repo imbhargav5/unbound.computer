eb8dd2328866910e46e21199147cf79cba3da036
Ponnapalli Bhargav <bhargavponnapalli.5@gmail.com>
2026-03-12 12:19:53 +0530

    Add macOS production build workflow (#96)

--- apps/web/src/components/navigation-menu/external-navbar/download-button.tsx
+++ apps/web/src/components/navigation-menu/external-navbar/download-button.tsx
@@ -1,20 +1,19 @@
-"use client";
-
 import { Download } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { macOsDownloadUrl } from "./constants";
 
 export function DownloadButton() {
   return (
     <Button
+      asChild
       variant="outline"
       size="sm"
       className="border-white/20 bg-white/5 text-white hover:bg-white/10 hover:text-white"
-      onClick={() => {
-        alert("Coming soon!");
-      }}
     >
-      <Download className="size-4" />
-      Download
+      <a aria-label="Download Unbound for Apple Silicon macOS" href={macOsDownloadUrl}>
+        <Download className="size-4" />
+        Download
+      </a>
     </Button>
   );
 }
